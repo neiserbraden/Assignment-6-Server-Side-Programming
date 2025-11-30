e5eaf6aa32c3ba7f02bf464fd47c34157fe7b9f4
neiserbraden <neiserbraden@gmail.com>
2025-11-30 18:20:15 -0500

    Update contacts.php

--- admin/contacts/contacts.php
+++ admin/contacts/contacts.php
@@ -1,1 +1,31 @@
+<?php
 
+
+function contacts_get($id) {
+    $items = contacts_load();
+    foreach ($items as $item) {
+        if ($item["id"] == $id) return $item;
+    }
+    return null;
+}
+
+function contacts_create($data) {
+    $items = contacts_load();
+
+    
+    $nextId = empty($items) ? 1 : max(array_column($items, "id")) + 1;
+
+    $newItem = [
+        "id" => $nextId,
+        "name" => $data["name"],
+        "email" => $data["email"],
+        "message" => $data["message"],
+        "created_at" => date("Y-m-d H:i:s")
+    ];
+
+    $items[] = $newItem;
+    contacts_save($items);
+
+    return $nextId;
+}
+?>
