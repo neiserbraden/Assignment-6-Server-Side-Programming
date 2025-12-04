3bf0da56c64fb5895c9eeafaa74e9975f6804a8a
neiserbraden <neiserbraden@gmail.com>
2025-12-03 23:02:55 -0500

    Update index.php

--- admin/products/index.php
+++ admin/products/index.php
@@ -1,32 +1,19 @@
 <?php
-require_once "products.php";
-$products = get_all_products();
+require_once "../classes/ProductManager.php";
+$products = productmanager::getAll();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Products Admin</title>
-</head>
-<body>
-    <h1>Products</h1>
+<h1>Products</h1>
 
-    <a href="create.php"> Add New Product</a>
-    <hr>
+<a href="create.php">Add New Product</a>
+<hr>
 
-    <?php if (empty($products)) : ?>
-        <p>No products found.</p>
-    <?php else: ?>
-        <ul>
-        <?php foreach ($products as $index => $p): ?>
-            <li>
-                <strong><?= htmlspecialchars($p['title']) ?></strong>
-                <br>
-                <a href="view.php?id=<?= $index ?>">View</a>
-                <a href="edit.php?id=<?= $index ?>">Edit</a> 
-                <a href="delete.php?id=<?= $index ?>">Delete</a>
-            </li>
-        <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
-</body>
-</html>
+<ul>
+<?php foreach ($products as $id => $p): ?>
+    <li>
+        <strong><?= htmlspecialchars($p['title']) ?></strong>
+        <a href="view.php?id=<?= $id ?>">View</a>
+        <a href="edit.php?id=<?= $id ?>">Edit</a>
+        <a href="delete.php?id=<?= $id ?>">Delete</a>
+    </li>
+<?php endforeach; ?>
+</ul>
