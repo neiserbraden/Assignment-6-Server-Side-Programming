dc88fc96131dcc8e70708ba82e7ab06ecb0098ec
neiserbraden <neiserbraden@gmail.com>
2025-12-03 23:04:11 -0500

    Update delete.php

--- admin/products/delete.php
+++ admin/products/delete.php
@@ -1,34 +1,18 @@
 <?php
-require_once "products.php";
+require_once "../classes/productmanager.php";
 
-$id = $_GET['id'] ?? null;
-$product = get_product($id);
+$id = $_GET['id'];
+$product = ProductManager::get($id);
 
-if (!$product) {
-    die("Product not found.");
-}
+if (!$product) die("Not found");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    delete_product($id);
+    ProductManager::delete($id);
     header("Location: index.php");
     exit;
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Delete Product</title>
-</head>
-<body>
 <h1>Delete Product</h1>
 
-<p>Are you sure you want to delete <strong><?= htmlspecialchars($product['title']) ?></strong>?</p>
+<p>Are you sure you want to delete <b><?= $product['title'] ?></b>?</p>
 
-<form method="post">
-    <button type="submit">Yes, delete</button>
-</form>
-
-<br>
-<a href="view.php?id=<?= $id ?>">Cancel</a>
-</body>
-</html>
