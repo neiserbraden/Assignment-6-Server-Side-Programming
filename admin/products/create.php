00cf2ad77d5cc14a837eabdd4e8013836bf5e561
neiserbraden <neiserbraden@gmail.com>
2025-12-03 23:03:20 -0500

    Update create.php

--- admin/products/create.php
+++ admin/products/create.php
@@ -1,34 +1,23 @@
 <?php
-require_once "products.php";
+require_once "../classes/productmanager.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    create_product($_POST['title'], $_POST['description'], $_POST['icon']);
+    $p = new Product($_POST['title'], $_POST['description'], $_POST['icon']);
+    ProductManager::create($p);
     header("Location: index.php");
     exit;
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Create Product</title>
-</head>
-<body>
-<h1>Add New Product</h1>
-
 <form method="post">
-    <label>Title</label><br>
-    <input type="text" name="title" required><br><br>
+    <label>Title</label>
+    <input name="title" required>
 
-    <label>Description</label><br>
-    <textarea name="description" required></textarea><br><br>
+    <label>Description</label>
+    <textarea name="description" required></textarea>
 
-    <label>Icon (default: layers)</label><br>
-    <input type="text" name="icon" value="layers"><br><br>
+    <label>Icon</label>
+    <input name="icon" value="layers">
 
     <button type="submit">Create</button>
 </form>
 
-<br>
-<a href="index.php">Back</a>
-</body>
-</html>
